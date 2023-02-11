6ce143cabba1071c85fc06783270cd2dd5372456
Дмитрий <tehnosd79@gmail.com>
2023-02-11 16:31:52 +0700

    Исправлено формирование отправки УПД

--- sendUPD.php
+++ sendUPD.php
@@ -19,48 +19,21 @@ $conn_update = new \backend\Connection(\backend\Connection::GD);
 $addDocument = false;
 $sendMailMyOnly = true;
 
-
-/*
-if ($addDocument === true){
-    // перейти в новый месяц (или Узнать новый месяц)
-//    $data = $conn->complexQuery("update S_B_K_CurentMonth set id_month = id_month+1");
-
-    //сформировать суммы по всем организациям для УПД
-
-    $data = $conn->complexQuery("
-insert into S_B_K_UniversalPaymentDocument (ORG
-      ,id_month
-      ,invoicedAamount)
-SELECT     ORG, id_month, CASE WHEN itogo < s_min THEN s_min WHEN itogo > s_max THEN s_max ELSE itogo END AS invoicedAamount
-FROM         (SELECT     ORG, summa, s_min, s_max, id_month, procent, ROUND(summa / 100 * procent, 2) AS itogo
-                       FROM          (SELECT     TOP (100) PERCENT dbo.accruals.ORG, SUM(dbo.accruals.summa) AS summa, dbo.S_B_K_MinMaxSumm.s_min,
-                                                                      dbo.S_B_K_MinMaxSumm.s_max, dbo.S_B_K_CurentMonth.id_month, dbo.S_B_K_MinMaxSumm.procent
-                                               FROM          dbo.accruals INNER JOIN
-                                                                      dbo.S_B_K_CurentMonth ON dbo.accruals.id_month = dbo.S_B_K_CurentMonth.id_month INNER JOIN
-                                                                      dbo.S_B_K_MinMaxSumm ON dbo.accruals.ORG = dbo.S_B_K_MinMaxSumm.ORG
-                                               WHERE      (dbo.accruals.id_level_totals = 30) AND (dbo.accruals.id_sub_level_accrual = 1)
-                                               GROUP BY dbo.accruals.ORG, dbo.S_B_K_MinMaxSumm.s_min, dbo.S_B_K_MinMaxSumm.s_max, dbo.S_B_K_CurentMonth.id_month,
-                                                                      dbo.S_B_K_MinMaxSumm.procent) AS t2) AS t1");
-
-
-}
-*/
-// сделать запрос сумм для УПД
-
 $data = $conn->complexQuery("
-SELECT     TOP (100) PERCENT dbo.S_B_K_UniversalPaymentDocument.ORG, dbo.S_B_K_UniversalPaymentDocument.id_month, dbo.S_B_K_UniversalPaymentDocument.id, 
-                      dbo.S_B_K_UniversalPaymentDocument.invoicedAamount, dbo.S_B_K_E_MailAddress.eMail, dbo.S_B_K_E_MailAddress.IO, dbo.ORG.name_AIS, 
-                      dbo.ORG.Contract_num, dbo.ORG.Contract_date, dbo.users.id AS id_user
-FROM         dbo.S_B_K_UniversalPaymentDocument INNER JOIN
-                      dbo.S_B_K_CurentMonth ON dbo.S_B_K_UniversalPaymentDocument.id_month = dbo.S_B_K_CurentMonth.id_month INNER JOIN
-                      dbo.S_B_K_E_MailAddress ON dbo.S_B_K_UniversalPaymentDocument.ORG = dbo.S_B_K_E_MailAddress.ORG INNER JOIN
-                      dbo.ORG ON dbo.S_B_K_UniversalPaymentDocument.ORG = dbo.ORG.ORG INNER JOIN
-                      dbo.users ON dbo.ORG.ORG = dbo.users.ORG
-WHERE     (dbo.S_B_K_E_MailAddress.send_PRO = 1) AND (dbo.users.login = 'SERVER')
+SELECT        TOP (100) PERCENT dbo.S_B_K_UniversalPaymentDocument.ORG, dbo.S_B_K_UniversalPaymentDocument.id_month, dbo.S_B_K_UniversalPaymentDocument.id, dbo.S_B_K_UniversalPaymentDocument.invoicedAamount, 
+                         dbo.S_B_K_E_MailAddress.eMail, dbo.S_B_K_E_MailAddress.IO, dbo.ORG.name_AIS, dbo.ORG.Contract_num, dbo.ORG.Contract_date, dbo.users.id AS id_user
+FROM            dbo.S_B_K_UniversalPaymentDocument INNER JOIN
+                             (SELECT        TOP (100) PERCENT MAX(id_month) AS id_month, ORG
+                               FROM            dbo.S_B_K_UniversalPaymentDocument AS S_B_K_UniversalPaymentDocument_1
+                               GROUP BY ORG) AS S_B_K_CurentMonth ON dbo.S_B_K_UniversalPaymentDocument.id_month = S_B_K_CurentMonth.id_month AND dbo.S_B_K_UniversalPaymentDocument.ORG = S_B_K_CurentMonth.ORG INNER JOIN
+                         dbo.S_B_K_E_MailAddress ON dbo.S_B_K_UniversalPaymentDocument.ORG = dbo.S_B_K_E_MailAddress.ORG INNER JOIN
+                         dbo.ORG ON dbo.S_B_K_UniversalPaymentDocument.ORG = dbo.ORG.ORG INNER JOIN
+                         dbo.users ON dbo.ORG.ORG = dbo.users.ORG
+WHERE        (dbo.S_B_K_E_MailAddress.send_PRO = 1) AND (dbo.users.login = 'SERVER')
 ");
-$_SESSION['id_month0'] = $conn_update->table('S_B_K_CurentMonth')->select('id_month')->fetchField('id_month');
 
 while ($res = $data->fetch()){
+    $_SESSION['id_month0'] = $res['id_month'];
     $_SESSION['ORG'] = $res['ORG'];
     $_SESSION['id_user'] = $res['id_user'];
     // Сформировать файл с по
