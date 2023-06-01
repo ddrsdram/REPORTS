5d2f73ad745b300d5cf0811c3de983806fb9fa06
Дмитрий <tehnosd79@gmail.com>
2023-06-01 15:33:22 +0700

    Создание отчета

---
+++ Reports/FIO/CertificateOfTheDeceased/VIEW.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rezzalbob
+ * Date: 20.04.2020
+ * Time: 16:00
+ */
+
+namespace Reports\FIO\CertificateOfTheDeceased;
+
+
+class VIEW extends \Reports\reportView
+{
+
+}
