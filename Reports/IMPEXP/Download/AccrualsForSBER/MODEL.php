8360a6627e0e68b0a4a3ac3a76b61fae49672e93
Дмитрий <tehnosd79@gmail.com>
2023-06-22 14:01:28 +0700

    Дополнително для сбера

--- Reports/IMPEXP/Download/AccrualsForSBER/MODEL.php
+++ Reports/IMPEXP/Download/AccrualsForSBER/MODEL.php
@@ -14,6 +14,7 @@ class MODEL extends \Reports\reportModel
     public function getDataArray()
     {
         $H = $this->getHeadArray();
+        // пришло с другой модели
         $this->conn->table("View_DAM_download_acruals_mail")
             ->where('id_user',$this->getUser());
 
