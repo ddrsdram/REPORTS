f5cef9a7ca5bd7ce288cf6568b9cbba4a2448c42
Администратор <tehnosd@mail.ru>
2026-03-25 17:40:13 +0700

    Починка отчета о перерасчете отоплния

---
+++ DB/View/View_GHR_autoCalc_svod.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace DB\View;
+
+
+use \DB\Connection;
+
+class View_GHR_autoCalc_svod extends Connection
+{
+	const id_user =  'id_user';
+
+	const id_month =  'id_month';
+
+	const ORG =  'ORG';
+
+	const region =  'region';
+
+    const name_region =  'name_region';
+
+    const id_street =  'id_street';
+
+	const status_street =  'status_street';
+
+	const name_street =  'name_street';
+
+	const house =  'house';
+
+	const V_odn =  'V_odn';
+
+	const V_UL =  'V_UL';
+
+	const V_new =  'V_new';
+
+	const znak =  'znak';
+
+	const calculate =  'calculate';
+
+	const area =  'area';
+
+	const area_UL =  'area_UL';
+
+	const standard =  'standard';
+
+	const new_standard =  'new_standard';
+
+	const full_name =  'full_name';
+
+	const value4 =  'value4';
+
+	const value5 =  'value5';
+
+	const value6 =  'value6';
+
+	const house_int =  'house_int';
+
+	const id_month_create =  'id_month_create';
+
+	const name_JEU =  'name_JEU';
+
+}
